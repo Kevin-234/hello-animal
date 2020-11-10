c130edb4ce649d7a6edc0ccf86dd293f4cb4a949
Akari Taguchi <kevintaguchi0@gmail.com>
2020-11-10 15:48:14 -0700

    fixed

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ db.once('open', function() {
 app.get('/animals', function(request, response){
   Animal.find(function(err, animals) {
     console.log(animals);
-    response.render('./pages/animals',{animals: animals})
+    response.render('./views/pages/animals',{animals: animals})
   });
 })
 
